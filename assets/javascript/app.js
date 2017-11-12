8d29d83b43f8bd972a98b1f9754f72277fd4950c
jsalamat <salamat_jon@yahoo.com>
2017-11-11 22:51:27 -0500

    for dont have sizing we put or nothing to avoid undefined

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,12 +6,16 @@ const inputs = document.querySelectorAll('.controls input');
 // This will return a node list and in it we want to loop the forEach method 
 // that was recently added to node list
 function handleUpdate() {
-	console.log(this.value);
+	//console.log(this.value);
 	// To get the value out of this by getting the suffix
 	// this reason there is a data-sizing which is a data attribute
 	// prefix "data-PLACEsuffixvalue" ex data-sizing="px"
 	// dataset is the object that will contain all the data attribute from specifix element
-	console.log(this.dataset)
+	//console.log(this.dataset);
+	// data- select everything from that element and put into an object
+	const suffix = this.dataset.sizing || '';
+	// for some that dont have sizing we put or nothing to avoid append undefine at the end
+	console.log(suffix);
 
 
 }
