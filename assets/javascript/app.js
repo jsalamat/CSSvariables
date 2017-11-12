9555c1c363c4d908694e03316bec48e705e5de98
jsalamat <salamat_jon@yahoo.com>
2017-11-11 21:56:01 -0500

    listen for a change event on each of the inputs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,3 +5,12 @@
 const inputs = document.querySelectorAll('.controls input');
 // This will return a node list and in it we want to loop the forEach method 
 // that was recently added to node list
+function handleUpdate() {
+	console.log(this.value);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+// listen for a change event on each of the inputs
+// loop over the forEach input, addEventListener to listen
+// for the change in the event
+// and when its called its going to handleUpdate
