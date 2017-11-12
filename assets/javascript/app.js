cb4d2b3b5e6ab2d2d9c2d1441c921df96ed91310
jsalamat <salamat_jon@yahoo.com>
2017-11-11 21:37:37 -0500

    app js to update the css variables by selecting inputs

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+// Use Javascript to update the CSS Variables by Selecting the inputs
+
+// This is going to select all the inputs in the page
+
+const inputs = document.querySelectorAll('.controls input');
+// This will return a node list and in it we want to loop the forEach method 
+// that was recently added to node list
